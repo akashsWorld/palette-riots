ab7c82e2f2fc3434e2c2e4dd06885108d6a0f991
Akash Biswas <akashbiswas.271999@gmail.com>
2025-03-24 11:22:25 +0530

    The button is done but the animation is not working because the no color properties are used only background image properti is been used here.

--- components/Buttton.tsx
+++ components/Buttton.tsx
@@ -3,17 +3,20 @@ import React from "react";
 const Buttton = ({
   buttonName,
   className,
-  targetUrl
+  targetUrl,
 }: {
   buttonName: string;
   targetUrl?: string;
   className?: string;
 }) => {
   return (
-    <div
-      className={`rounded-[20px] p-[1px] min-h-fit min-w-auto gradient-bg transition-all duration-300 ease-in-out ${className}`}
+    <div className="gradient-bg p-[1px] rounded-[20px]">
+    <a
+      href={targetUrl}
+      className={`btn ${className}`}
     >
-      <a href={targetUrl} className="flex items-center justify-center h-full rounded-[20px] bg-white bg-clip-content hover:bg-clip-text hover:text-white transition-all duration-300 ease-in-out"><p className="w-fit text-inherit p-[10px]">{buttonName}</p></a>
+      {buttonName}
+    </a>
     </div>
   );
 };
