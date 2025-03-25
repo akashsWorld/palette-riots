bc4d7c3298e7f07f60f6bfd233edd00ebbb86c71
Akash Biswas <akashbiswas.271999@gmail.com>
2025-03-25 18:54:28 +0530

    Add the static page configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
-  // basePath: "/host-site",
+  output: "export",
   /* config options here */
 };
 
